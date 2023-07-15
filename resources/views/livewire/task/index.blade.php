fb198240e6af2c21c3b484675e81c2ac1c976f11
lamkimphu258 <lamkimphu258@gmail.com>
2023-07-15 12:53:15 +0700

    Feature: Improve UI

--- resources/views/livewire/task/index.blade.php
+++ resources/views/livewire/task/index.blade.php
@@ -4,9 +4,9 @@
     </h2>
     @foreach($unFinishedTasks as $task)
     @if (!$task->is_finished)
-    <div class="px-4 py-2 text-justify gap-5 bg-white rounded-lg mb-2 grid grid-cols-12 h-[60px] hover:border hover:border-lime-500 hover:cursor-pointer items-center" wire:key="task-{{ $task->id }}" :class="isFinished && 'hidden'">
-        <p class="grow col-span-11">{{ $task->title }}</p>
-        <button wire:click="finish({{$task->id}})" class="rounded-lg bg-lime-500 hover:bg-lime-700 px-4 col-span-1 h-full">{{ $task->is_finished ? '' : 'Done'}}</button>
+    <div class="px-4 py-2 text-justify gap-5 bg-white rounded-lg mb-2 grid grid-cols-12 min-h-[60px] hover:border hover:border-lime-500 hover:cursor-pointer items-center" wire:key="task-{{ $task->id }}" :class="isFinished && 'hidden'">
+        <p class="grow col-span-8 sm:col-span-11 break-all">{{ $task->title }}</p>
+        <button wire:click="finish({{$task->id}})" class="rounded-lg bg-lime-500 hover:bg-lime-700 px-4 col-span-4 sm:col-span-1 h-[50px]">{{ $task->is_finished ? '' : 'Done'}}</button>
     </div>
     @endif
     @endforeach
@@ -16,9 +16,9 @@
     </h2>
     @foreach($finishedTasks as $task)
     @if ($task->is_finished)
-    <div class="px-4 py-2 text-justify gap-5 bg-white rounded-lg mb-2 grid grid-cols-12 h-[60px] hover:border hover:border-lime-500 hover:cursor-pointer items-center" wire:key="task-{{ $task->id }}">
-        <p class="grow col-span-11">{{ $task->title }}</p>
-        <button wire:click="undo({{$task->id}})" class="rounded-lg bg-lime-500 hover:bg-lime-700 px-4 col-span-1 h-full">{{ $task->is_finished ? 'Undo' : ''}}</button>
+    <div class="px-4 py-2 text-justify gap-5 bg-white rounded-lg mb-2 grid grid-cols-12 min-h-[60px] hover:border hover:border-lime-500 hover:cursor-pointer items-center" wire:key="task-{{ $task->id }}">
+        <p class="grow col-span-8 sm:col-span-11 break-all">{{ $task->title }}</p>
+        <button wire:click="undo({{$task->id}})" class="rounded-lg bg-lime-500 hover:bg-lime-700 px-4 col-span-4 sm:col-span-1 h-full">{{ $task->is_finished ? 'Undo' : ''}}</button>
     </div>
     @endif
     @endforeach
